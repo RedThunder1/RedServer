ba3eb86c6e9ca660e058b42aa1aa39f5d8fc0871
RedThunder1 <redthunderspartan@gmail.com>
2021-06-28 20:20:45 -0500

    Initial commit

---
+++ src/main/java/redserver/redserver/RedMain.java
@@ -0,0 +1,33 @@
+package redserver.redserver;
+
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RedMain extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        loadCommands();
+        loadEvents();
+        getServer().getConsoleSender().sendMessage(ChatColor.RED + "[!] RedSpigot Has been ENABLED [!]");
+    }
+
+    @Override
+    public void onDisable() {
+        getServer().getConsoleSender().sendMessage(ChatColor.RED + "[!] RedSpigot Has been DISABLED [!]");
+    }
+
+    public void onLoad() {
+        getServer().getConsoleSender().sendMessage(ChatColor.RED + "[!] RedSpigot Has been LOADED [!]");
+    }
+
+    public void loadCommands() {
+        this.getCommand("smp").setExecutor(new smptp());
+        this.getCommand("hub").setExecutor(new hub());
+    }
+
+    public void loadEvents() {
+
+    }
+
+}
---
+++ src/main/java/redserver/redserver/hub.java
@@ -0,0 +1,38 @@
+package redserver.redserver;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class hub implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+
+        if (!(sender instanceof Player)) {
+            sender.sendMessage(ChatColor.RED + "Only players can use this command!");
+            return false;
+        }
+        Player player = (Player) sender;
+        if (args.length == 0) {
+            player.sendMessage(ChatColor.RED + "Please enter a world to teleport to!");
+            return false;
+        }
+
+        World world = Bukkit.getWorld("world");
+        Location location = new Location(world, 0.5, 77, 0.5);
+
+        if (player.getWorld() == world) {
+            player.sendMessage(ChatColor.RED + "You are already in this World!");
+            return false;
+        }
+
+        player.teleport(location);
+
+        return false;
+    }
+}
